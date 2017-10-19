95f2a940f75705a6ad8e465b0481bec4d4470171
Richard Goater <richard.goater27@gmail.com>
2017-10-19 17:02:07 +0100

    Add cgmlst task

---
+++ cgmlst/formatter.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+
+function getSource(url) {
+  if (/pubmlst\.org/.test(url)) return 'PubMLST';
+  if (/pasteur\.fr/.test(url)) return 'Pasteur';
+  if (/warwick\.ac\.uk/.test(url)) return 'EnteroBase';
+  return url;
+}
+
+function format({ st, url, genes, alleles }) {
+  const matches = [];
+
+  for (const gene of genes) {
+    for (const hit of alleles[gene]) {
+      matches.push(Object.assign({ gene }, hit));
+    }
+  }
+
+  return {
+    st,
+    url,
+    source: getSource(url),
+    matches,
+  };
+}
+
+function read(stdin) {
+	var ret = '';
+
+	return new Promise(function (resolve) {
+		if (stdin.isTTY) {
+			resolve(ret);
+			return;
+		}
+
+		stdin.setEncoding('utf8');
+
+		stdin.on('readable', function () {
+			var chunk;
+
+			while ((chunk = stdin.read())) {
+				ret += chunk;
+			}
+		});
+
+		stdin.on('end', function () {
+			resolve(ret);
+		});
+	});
+};
+
+read(process.stdin)
+  .then(JSON.parse)
+  .then(format)
+  .then(JSON.stringify)
+  .then(console.log)
+  .catch(e => {
+    console.error(e);
+    process.exit(1);
+  })
