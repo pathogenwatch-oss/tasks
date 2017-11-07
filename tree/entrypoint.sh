06b4b181dd490ddd90261bb22b0c6f3a63889218
Richard Goater <richard.goater27@gmail.com>
2017-11-07 15:25:55 +0000

    Working tree image

--- tree/entrypoint.sh
+++ tree/entrypoint.sh
@@ -2,15 +2,12 @@
 
 set -e
 
-mkdir -p /data
-
 cd /wgsa
 
-cat - | node /wgsa/input.js
+node /wgsa/input.js
 
 cd /data
 
-/tree_pipeline.sh -s $WGSA_ORGANISM_TAXID -i .
+/tree_pipeline.sh -s $WGSA_ORGANISM_TAXID -i . > /dev/null
 
-echo { \"tree\": \"$(cat wgsa.nwk)\" }
-# cat /data/5a009f2d5d851d772e25483a_core.jsn
+echo { \"tree\": \"$(cat wgsa.nwk)\", \"matrix\": \"$(cat result.mldist)\" }
--- tree/input.js
+++ tree/input.js
@@ -3,16 +3,20 @@ const fs = require('fs');
 
 process.stdin
   .pipe(es.split())
-  .on('data', data => {
+  .pipe(es.map((data, done) => {
     if (data.length > 1) {
       const json = JSON.parse(data);
       delete json.analysis.core.__v;
       json.analysis.core.coreProfile.assemblyId = json._id;
       json.analysis.core.coreSummary.assemblyId = json._id;
-      fs.writeFileSync(
+      fs.writeFile(
         `/data/${json._id}_core.jsn`,
-        JSON.stringify(json.analysis.core)
+        JSON.stringify(json.analysis.core),
+        done
       );
+    } else {
+      done(null);
     }
-  })
-  .on('end', () => console.log('end'));
+  }))
+  .on('error', console.error)
+  .on('end', () => console.error('end'));
