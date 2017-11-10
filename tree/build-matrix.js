ae36d501085f9b4ed04d5b98dc13498dbeca12ea
KAD <kad0xf@gmail.com>
2017-11-10 17:12:14 +0000

    Paired matrix vectors

--- tree/build-matrix.js
+++ tree/build-matrix.js
@@ -5,7 +5,7 @@ const spawn = require('child_process').spawn;
 const mapLimit = require('async/mapLimit');
 
 const dataPath = '.';
-const limit = 100;
+const limit = process.env.WGSA_WORKERS || 8;
 
 function saveProfiles(stream) {
   return new Promise((resolve, reject) => {
@@ -16,14 +16,16 @@ function saveProfiles(stream) {
       .pipe(es.map((data, done) => {
         if (data.length > 1) {          
           const json = JSON.parse(data);
-          ids.push(json._id);
-          delete json.analysis.core.__v;
-          // json.analysis.core.coreProfile.assemblyId = json._id;
-          // json.analysis.core.coreSummary.assemblyId = json._id;
+          delete json.analysis.core.__v;          
           const coreProfile = JSON.stringify(json.analysis.core);
+          // const coreProfile = data;
+          
+          ids.push(json._id.$oid ? json._id.$oid : json._id);
+          // ids.push(`leaf${coreProfiles.length}`);
+
           const file = path.join(dataPath, `${coreProfiles.length}_core.jsn`);
-          coreProfiles.push(file);
           fs.writeFile(file, coreProfile, done);
+          coreProfiles.push(file);
         } else {
           done(null);
         }
@@ -51,19 +53,66 @@ function compareProfiles(item, coreProfiles, done) {
   });
 }
 
+  if (index > 0) {
+    compareProfiles(item, coreProfiles.slice(0, index), done);          
+  } else {
+    done(null, []);
+  }
+}
+
+function pairProfiles(coreProfiles) {
+  const pairs = [];
+  for (var index = 0; index < coreProfiles.length / 2; index++) {
+    const coIndex = coreProfiles.length - 1 - index;
+    if (index === coIndex) {
+      pairs.push([ coreProfiles[index] ]);
+    } else {
+      pairs.push([ 
+        coreProfiles[index],
+        coreProfiles[coreProfiles.length - 1 - index],
+      ]);
+    }
+  }
+  return pairs;
+}
+
+function flatenPairedResults(results) {
+  const matrix = [];
+  for (var index = 0; index < results.length; index++) {
+    matrix.push(results[index][0]);
+  }
+  for (var index = results.length - (results[results.length - 1].length === 1 ? 2 : 1); index >= 0; index--) {
+    matrix.push(results[index][1]);
+  }
+  return matrix;
+}
+
 function buildMatrix({ ids, coreProfiles }) {
   return new Promise((resolve, reject) => {
     mapLimit(
-      coreProfiles, 
+      pairProfiles(coreProfiles), 
       limit, 
-      (item, done) => {
-        compareProfiles(item, coreProfiles, done);
+      (pair, done) => {
+        compareProfile(coreProfiles, pair[0], (err0, vector0) => {
+          if (err0) return done(err0);
+          if (pair.length === 1) return done(null, [ vector0 ]);
+          compareProfile(coreProfiles, pair[1], (err1, vector1) => {
+            if (err1) return done(err1);
+            done(null, [ vector0, vector1 ]);
+          });
+        });
+        // const index = coreProfiles.indexOf(item);
+        // if (index > 0) {
+        //   compareProfiles(item, coreProfiles.slice(0, index), done);          
+        // } else {
+        //   done(null, []);
+        // }
       }, 
-      (err, matrix) => {
+      (err, results) => {
         if (err) {
           reject(err);
         } else {
-          resolve({ ids, matrix });
+          resolve({ ids, matrix: flatenPairedResults(results) });
         }
       }
     );
@@ -84,7 +133,7 @@ function buildTree({ ids, matrix }) {
     for (let index = 0; index < matrix.length; index++) {
       inputFile.write(labels[index]);  
       inputFile.write('\t');
-      for(let buffer of matrix[index]) {
+      for(const buffer of matrix[index]) {
         inputFile.write(buffer, 'utf8');
       }
       inputFile.write('\n');  
@@ -97,4 +146,4 @@ Promise.resolve(process.stdin)
   .then(saveProfiles)
   .then(buildMatrix)
   .then(buildTree)
-  .catch(console.error)
+  .catch(err => console.error('ERROR') || console.error(err));
