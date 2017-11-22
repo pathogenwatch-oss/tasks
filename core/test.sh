632c535636d639ae8f47bb08bb34d175028048c5
KAD <kad0xf@gmail.com>
2017-11-22 10:49:25 +0000

    Use core-fp v0.0.3

--- core/test.sh
+++ core/test.sh
@@ -2,14 +2,21 @@
 
 set -e
 
-for file in $1/*.fasta
+for file in $1/fastas/*.fasta
 do
-  if [ ! -f $1/$(basename $file).core.json ]; then
-    echo $file
+  echo $file
+  mkdir -p $1/cores
+  if [ ! -f $1/cores/$(basename $file .fasta).json ]; then
     cat $file | \
       docker run -i --rm \
         -e WGSA_ORGANISM_TAXID=1280 \
         -e WGSA_FILE_ID=`shasum $file | cut -d ' ' -f 1` \
-        registry.gitlab.com/cgps/wgsa-tasks/core:v1 > $1/$(basename $file).core.json
+        registry.gitlab.com/cgps/wgsa-tasks/core:t2 > $1/cores/$(basename $file .fasta).json
+  fi
+  mkdir -p $1/variancedata  
+  if [ ! -f $1/variancedata/$(basename $file .fasta).json ]; then
+    if [ ! -f $1/variancedata/$(basename $file .fasta).bson ]; then
+      node core-to-variancedata.js  $1/cores/$(basename $file .fasta).json  $1/variancedata/$(basename $file .fasta).json  $1/variancedata/$(basename $file .fasta).bson
+    fi
   fi
 done
