a99faf37df0ffcfab436ce5c8364218da022cbf0
Corin Yeats <corin.yeats@gmail.com>
2018-05-18 13:05:08 +0100

    Made core building parallelisable.

--- test-tree.sh
+++ test-tree.sh
@@ -23,17 +23,10 @@ mkdir -p inputs
 
 find ${FASTA_DIR} -maxdepth 1 -name "*${FASTA_SUFFIX}" -print0 | xargs -0 -I infile -P ${NUM_PROCS} sh -c 'echo "$1 , $2 , $3 , $4" && cat "$1" | docker run -i -e WGSA_ORGANISM_TAXID="$2" registry.gitlab.com/cgps/wgsa-tasks/core:"$3" > inputs/$(basename "$1" "$4").json' -- infile ${ORGANISM_TAXID} ${TEST_VERSION} ${FASTA_SUFFIX}
 
-#
-#for FILE in $(ls $FASTA_DIR/*$FASTA_SUFFIX)
-#do
-#  cat $FILE | docker run -i \
-#    -e WGSA_ORGANISM_TAXID=$ORGANISM_TAXID \
-#    registry.gitlab.com/cgps/wgsa-tasks/core:$TEST_VERSION > inputs/$(basename -s $FASTA_SUFFIX $FILE).json
-#done
-
 CORES=$(ls inputs/*.json)
 
 node create-bson-tree-input.js $CORES > input.bson
+
 rm -rf inputs
 
 cat input.bson | docker run -i \
