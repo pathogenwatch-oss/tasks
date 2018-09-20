36c8102a504dedfcd7f68c55bd01728694755108
Corin Yeats <corin.yeats@gmail.com>
2018-09-20 14:36:20 +0100

    Simplified kleborate build as it's just a rename.

--- kleborate/build.sh
+++ kleborate/build.sh
@@ -2,5 +2,7 @@
 
 version="v0.9.1"
 
+docker pull registry.gitlab.com/cgps/cgps-kleborate:${version}
+
 docker tag registry.gitlab.com/cgps/cgps-kleborate:${version} registry.gitlab.com/cgps/wgsa-tasks/kleborate:${version} \
   && docker push registry.gitlab.com/cgps/wgsa-tasks/kleborate:${version}
