c9decf0b6db769136904360ce2cf004ebaedf928
Corin Yeats <corin.yeats@gmail.com>
2018-12-17 09:49:47 +0000

    Updated genotyphi to new version.

--- genotyphi/build.sh
+++ genotyphi/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build -t registry.gitlab.com/cgps/wgsa-tasks/genotyphi:v2.0.2 . \
-  && docker push registry.gitlab.com/cgps/wgsa-tasks/genotyphi:v2.0.2
+docker build -t registry.gitlab.com/cgps/wgsa-tasks/genotyphi:v2.0.3 . \
+  && docker push registry.gitlab.com/cgps/wgsa-tasks/genotyphi:v2.0.3
