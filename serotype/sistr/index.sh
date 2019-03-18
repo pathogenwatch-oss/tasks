8dc3ffe29b95aff581bb5eec47faa3628abffcfc
Richard Goater <richard.goater27@gmail.com>
2019-03-18 19:56:43 +0000

    [serotype] Format source string

--- serotype/sistr/index.sh
+++ serotype/sistr/index.sh
@@ -3,4 +3,4 @@
 cat - > /tmp/input.fasta
 
 /NGStools/miniconda3/bin/sistr --qc /tmp/input.fasta | \
-  /opt/pathogenwatch/jq-linux64 '{ value: .[0].serovar, subspecies: .[0].cgmlst_subspecies, source: "sistr" }'
+  /opt/pathogenwatch/jq-linux64 '{ value: .[0].serovar, subspecies: .[0].cgmlst_subspecies, source: "SISTR" }'
