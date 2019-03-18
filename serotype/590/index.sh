e4f0fc17f8a213c61a70ab955c5c3ab9d5068111
Richard Goater <richard.goater27@gmail.com>
2019-03-18 19:44:36 +0000

    [Serotype] Update output format

--- serotype/590/index.sh
+++ serotype/590/index.sh
@@ -2,4 +2,5 @@
 
 cat - > /tmp/input.fasta
 
-/NGStools/miniconda3/bin/sistr --qc /tmp/input.fasta | /opt/pathogenwatch/jq-linux64 '{ serovar: .[0].serovar }'
+/NGStools/miniconda3/bin/sistr --qc /tmp/input.fasta | \
+  /opt/pathogenwatch/jq-linux64 '{ value: .[0].serovar, subspecies: .[0].cgmlst_subspecies, source: "sistr" }'
