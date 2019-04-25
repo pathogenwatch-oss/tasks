d49f470e0084da8f3818d5016bcd1b53c952fb20
Richard <richard.goater27@gmail.com>
2019-04-25 15:42:52 +0000

    Format result in PW-specific entrypoint

---
+++ serotype/seroba/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+result=$(cat - | /root/bin/run_seroba.sh)
+jq --arg key0 'source' \
+   --arg value0 'SeroBA' \
+   --arg key1 'value' \
+   --arg value1 $result \
+    '. | .[$key0]=$value0 | .[$key1]=$value1 ' \
+   <<<'{}'
