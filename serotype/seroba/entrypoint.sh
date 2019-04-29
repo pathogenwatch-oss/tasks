e051e102436aeb8f78c06b192240dd912f7d1052
Richard <richard.goater27@gmail.com>
2019-04-29 10:54:26 +0000

    Handle spaces in output

--- serotype/seroba/entrypoint.sh
+++ serotype/seroba/entrypoint.sh
@@ -4,6 +4,6 @@ result=$(cat - | /root/bin/run_seroba.sh)
 jq --arg key0 'source' \
    --arg value0 'SeroBA' \
    --arg key1 'value' \
-   --arg value1 $result \
+   --arg value1 "$result" \
     '. | .[$key0]=$value0 | .[$key1]=$value1 ' \
    <<<'{}'
