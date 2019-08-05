ab7573c08983f76b8f0863456581343671e1c1ac
Richard Goater <richard.goater27@gmail.com>
2019-08-05 11:49:44 +0100

    Update warning results

--- serotype/seroba/entrypoint.sh
+++ serotype/seroba/entrypoint.sh
@@ -2,7 +2,7 @@
 
 export PIRS_COVERAGE=50
 result=$(cat - | /root/bin/run_seroba.sh)
-warn=$([[ "$result" =~ ^(06D|11E|35A)$ ]] && echo 'true' || echo 'false')
+warn=$([[ "$result" =~ ^(untypable|32F|33A/33F|11E|19F|32A|35A|06A|possible 6(A|C|D|E)$ ]] && echo 'true' || echo 'false')
 jq -n \
    --arg value "$result" \
    --argjson warn $warn \
