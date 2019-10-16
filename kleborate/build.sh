08948c1c35775f45247b7a260861b31e0a576c6a
Corin Yeats <corin.yeats@gmail.com>
2019-10-16 16:06:54 +0100

    Updated the kleborate container build script.

--- kleborate/build.sh
+++ kleborate/build.sh
@@ -4,5 +4,5 @@ version="v2.0.0"
 
 docker pull registry.gitlab.com/cgps/cgps-kleborate:${version}
 
-docker tag registry.gitlab.com/cgps/cgps-kleborate:${version} registry.gitlab.com/cgps/wgsa-tasks/kleborate:${version} \
-  && docker push registry.gitlab.com/cgps/wgsa-tasks/kleborate:${version}
+docker tag registry.gitlab.com/cgps/cgps-kleborate:${version} registry.gitlab.com/cgps/pathogenwatch-tasks/kleborate:${version} \
+  && docker push registry.gitlab.com/cgps/pathogenwatch-tasks/kleborate:${version}
