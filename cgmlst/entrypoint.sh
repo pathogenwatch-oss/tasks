a7013d197512c432f4f3bd2120ccf5c92884d6cb
Ben Taylor <ben.taylor@sanger.ac.uk>
2019-11-08 13:42:58 +0100

    Translate PW env variables for (cg)mlst

--- cgmlst/entrypoint.sh
+++ cgmlst/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+export ORGANISM_TAXID=$PW_ORGANISM_TAXID
+export SPECIES_TAXID=$PW_SPECIES_TAXID
+export GENUS_TAXID=$PW_GENUS_TAXID
+
 /usr/local/bin/node --max-old-space-size=4096 /usr/local/mlst/index.js | /usr/local/bin/node /wgsa/formatter.js
