9af402f122803f600568e23a417581583b9f19ed
Ben Taylor <ben.taylor@sanger.ac.uk>
2021-01-26 17:52:38 +0100

    Add SARS-CoV2 tasks

---
+++ pangolin/build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+version="v2.1.7_2021-01-20"
+
+docker pull registry.gitlab.com/cgps/cog-uk/pangolin:${version}
+
+docker tag registry.gitlab.com/cgps/cog-uk/pangolin:${version} registry.gitlab.com/cgps/pathogenwatch-tasks/pangolin:${version} \
+  && docker push registry.gitlab.com/cgps/pathogenwatch-tasks/pangolin:${version}
---
+++ sars_cov2_variants/build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+version="0.0.7"
+
+docker pull registry.gitlab.com/cgps/cog-uk/type_variants:${version}
+
+docker tag registry.gitlab.com/cgps/cog-uk/type_variants:${version} registry.gitlab.com/cgps/pathogenwatch-tasks/sars_cov2_variants:${version} \
+  && docker push registry.gitlab.com/cgps/pathogenwatch-tasks/sars_cov2_variants:${version}
