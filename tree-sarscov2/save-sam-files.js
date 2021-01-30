b9deea60bbc7405ab6a23f11dbb14885f88a9b0e
Khalil <ka10@sanger.ac.uk>
2021-01-30 09:23:41 +0000

    Update save-sam-files.js

--- tree-sarscov2/save-sam-files.js
+++ tree-sarscov2/save-sam-files.js
@@ -4,27 +4,12 @@ const path = require('path');
 
 const samFolder = process.argv[2];
 
-function writeFile(file, data) {
-  return new Promise((resolve, reject) => {
-    fs.writeFile(file,
-      data,
-      (error) => {
-        if (error) {
-          reject(error);
-        }
-        else {
-          resolve();
-        }
-      },
-    );
-  });
-}  
-
 async function saveSamFiles(readStream) {
   const ids = [];
 
   for await (const chunk of readStream) {
     if (chunk.genomes) {
+      // this is header chunk
       for (const genome of chunk.genomes) {
         ids.push([
           genome._id.toString(),
@@ -33,9 +18,10 @@ async function saveSamFiles(readStream) {
       }
     }
     else {
+      // this is a core profile chunk
       const filePath = path.join(samFolder, `${chunk._id.toString()}.sam`);
       console.error('writing file', filePath, chunk.fileId);
-      await writeFile(filePath, chunk.analysis.core.sam);
+      await fs.promises.writeFile(filePath, chunk.analysis.core.sam);
     }
   }
 
@@ -45,6 +31,4 @@ async function saveSamFiles(readStream) {
 Promise.resolve(process.stdin)
   .then((stream) =>stream.pipe(new BsonStream()))
   .then(saveSamFiles)
-  .then(JSON.stringify)
-  .then(console.log)
   .catch(console.error);
