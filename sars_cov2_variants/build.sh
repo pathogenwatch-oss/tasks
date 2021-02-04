9ba8420c190d4b3ba1c11ecd9c8167f13256b79b
Khalil <ka10@sanger.ac.uk>
2021-02-04 13:02:56 +0000

    Rename task to match name in middle- and front-end.

--- sars_cov2_variants/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-version="0.0.8"
-
-docker pull registry.gitlab.com/cgps/cog-uk/type_variants:${version}
-
-docker tag registry.gitlab.com/cgps/cog-uk/type_variants:${version} registry.gitlab.com/cgps/pathogenwatch-tasks/sars_cov2_variants:${version} \
-  && docker push registry.gitlab.com/cgps/pathogenwatch-tasks/sars_cov2_variants:${version}
---
+++ sarscov2-variants/build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+version="0.0.8"
+
+docker pull registry.gitlab.com/cgps/cog-uk/type_variants:${version}
+
+docker tag registry.gitlab.com/cgps/cog-uk/type_variants:${version} registry.gitlab.com/cgps/pathogenwatch-tasks/sarscov2-variants:${version} \
+  && docker push registry.gitlab.com/cgps/pathogenwatch-tasks/sarscov2-variants:${version}
