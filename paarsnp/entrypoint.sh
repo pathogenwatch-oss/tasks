2609a61794b647c123b815bdddca767e5f079669
Corin Yeats <dr.yeats@gmail.com>
2021-05-18 10:12:51 +0000

    New version of PAARSNP for the updated integration

--- paarsnp/entrypoint.sh
+++ paarsnp/entrypoint.sh
@@ -2,4 +2,4 @@
 
 cat - > /tmp/sequence.fa
 
-java -jar /paarsnp/paarsnp.jar -i /tmp/sequence.fa -s ${PW_ORGANISM_TAXID} -s ${PW_SPECIES_TAXID} -s ${PW_GENUS_TAXID} -o | /usr/local/bin/node /wgsa/formatter.js
+java -jar /paarsnp/paarsnp.jar -i /tmp/sequence.fa -s ${PW_ORGANISM_TAXID} -s ${PW_SPECIES_TAXID} -s ${PW_GENUS_TAXID} -o
