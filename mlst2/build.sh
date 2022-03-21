628025b11dbb66a0c053011b840300e10db96207
corin.yeats <corin.yeats@cgps.group>
2022-03-21 12:06:52 +0000

    Added push for mls2 as well.

--- mlst2/build.sh
+++ mlst2/build.sh
@@ -7,3 +7,4 @@ docker build \
   --build-arg BASE=alternative-mlst-$VERSION \
   --build-arg TASK=mlst2 \
   -t registry.gitlab.com/cgps/pathogenwatch-tasks/mlst2:$VERSION .
+docker push registry.gitlab.com/cgps/pathogenwatch-tasks/mlst2:$VERSION
