b43170752423018cd91fd9c5441b09b177e7eaf2
corin.yeats <corin.yeats@cgps.group>
2022-05-16 11:20:03 +0100

    Fixed build script.

--- core/build.sh
+++ core/build.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
-if [ "$#" -ne 1 ]; then
+set -eu -o pipefail
+
+if [ "$#" -ne 2 ]; then
     echo "./build.sh [version e.g. v2.0.0] [species e.g. 1463165]"
+    exit 1
 fi
 
 VERSION=${1}
 SPECIES=${2}
 
-docker build --rm --build-arg species="${SPECIES}" --build-arg VERSION="${VERSION}" -t registry.gitlab.com/cgps/pathogenwatch-tasks/core:${VERSION}-${SPECIES} .
+docker build --rm --build-arg SPECIES="${SPECIES}" --build-arg BASE="${VERSION}" -t registry.gitlab.com/cgps/pathogenwatch-tasks/core:${VERSION}-${SPECIES} .
+docker push registry.gitlab.com/cgps/pathogenwatch-tasks/core:${VERSION}-${SPECIES}
