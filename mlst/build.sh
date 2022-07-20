aa3325699c06d7b67d0cc337360cf2f839706bb2
corin.yeats <corin.yeats@cgps.group>
2022-07-20 18:28:06 +0100

    Updated mlst and added docker push to build script.

--- mlst/build.sh
+++ mlst/build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 TASK=${1:-mlst}
-VERSION=${2:-202203171422-v3.0.2}
+VERSION=${2:-202203171422-v3.0.3}
 
 docker build \
   --build-arg BASE=$TASK-$VERSION \
   --build-arg TASK=$TASK \
   -t registry.gitlab.com/cgps/pathogenwatch-tasks/$TASK:$VERSION .
+
+docker push registry.gitlab.com/cgps/pathogenwatch-tasks/$TASK:$VERSION
