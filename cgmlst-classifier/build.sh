955f168f36910cec1f82694c4ad12cc4ffc5811e
corin.yeats <corin.yeats@cgps.group>
2022-10-10 15:07:27 +0100

    Removed dead code.

--- cgmlst-classifier/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-VERSION=1-13
-
-docker pull registry.gitlab.com/cgps/pathogenwatch/cgmlst-classifier:${VERSION}
-
-docker tag registry.gitlab.com/cgps/pathogenwatch/cgmlst-classifier:${VERSION} registry.gitlab.com/cgps/pathogenwatch-tasks/cgmlst-classifier:${VERSION}
-
-docker push registry.gitlab.com/cgps/pathogenwatch-tasks/cgmlst-classifier:${VERSION}
