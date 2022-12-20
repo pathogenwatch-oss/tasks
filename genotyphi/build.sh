1eba2ab8d3abf069dc258d0732225503dbd53677
corin.yeats <corin.yeats@cgps.group>
2022-12-20 16:44:17 +0000

    Updated current genotyphi version.

--- genotyphi/build.sh
+++ genotyphi/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build -t registry.gitlab.com/cgps/pathogenwatch-tasks/genotyphi:v3 . \
-  && docker push registry.gitlab.com/cgps/pathogenwatch-tasks/genotyphi:v3
+docker build -t registry.gitlab.com/cgps/pathogenwatch-tasks/genotyphi:v4 . \
+  && docker push registry.gitlab.com/cgps/pathogenwatch-tasks/genotyphi:v4
