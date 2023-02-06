6d0b6df5e99f8412049e3c0148a14b30b56368d7
corin.yeats <corin.yeats@cgps.group>
2023-02-06 16:39:12 +0000

    Updated the paarsnp build script.

--- paarsnp/build.sh
+++ paarsnp/build.sh
@@ -2,10 +2,10 @@
 
 set -euo pipefail
 
-AMRSEARCH_VERSION=${1:-v3.0.5}
+AMRSEARCH_VERSION=${1:-v3.0.12}
 PW_VERSION=${2:-${AMRSEARCH_VERSION}}
 
 
-docker build --build-arg VERSION="${AMRSEARCH_VERSION}" -t registry.gitlab.com/cgps/pathogenwatch-tasks/vista:"${PW_VERSION}" .
+docker build --build-arg VERSION="${AMRSEARCH_VERSION}" -t registry.gitlab.com/cgps/pathogenwatch-tasks/paarsnp:"${PW_VERSION}" .
 
-docker push registry.gitlab.com/cgps/pathogenwatch-tasks/vista:"${PW_VERSION}"
+docker push registry.gitlab.com/cgps/pathogenwatch-tasks/paarsnp:"${PW_VERSION}"
