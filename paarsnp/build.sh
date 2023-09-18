9bdfa399ea3a4978f5ece65a4511f4060da69294
Corin Yeats <corin.yeats@cgps.group>
2023-09-18 15:03:07 +0100

    Updated the AMR image version.

--- paarsnp/build.sh
+++ paarsnp/build.sh
@@ -2,10 +2,10 @@
 
 set -euo pipefail
 
-AMRSEARCH_VERSION=${1:-v3.0.20}
+AMRSEARCH_VERSION=${1:-v3.0.21}
 PW_VERSION=${2:-${AMRSEARCH_VERSION}}
 
 
 docker build --rm --build-arg VERSION="${AMRSEARCH_VERSION}" -t registry.gitlab.com/cgps/pathogenwatch-tasks/paarsnp:"${PW_VERSION}" .
 
-docker push registry.gitlab.com/cgps/pathogenwatch-tasks/paarsnp:"${PW_VERSION}"
+#docker push registry.gitlab.com/cgps/pathogenwatch-tasks/paarsnp:"${PW_VERSION}"
