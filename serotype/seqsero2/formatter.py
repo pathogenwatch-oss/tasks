81f91342df143c9aa7ba23e1668f1d3e7c036b6b
Corin Yeats <corin.yeats@cgps.group>
2023-10-06 14:30:19 +0100

    Fixed seqsero2 output to be compatible with the other serotyping tools.

--- serotype/seqsero2/formatter.py
+++ serotype/seqsero2/formatter.py
@@ -4,6 +4,7 @@ import sys
 sys.stdin.readline()
 sys.stdin.readline()
 
-print(json.dumps(
-    {record[0].strip(): record[1].strip() for record in
-     [line.split(":") for line in sys.stdin.readlines() if line.strip() != '']}))
+result = {"source": "SeqSero2"}
+result.update({record[0].strip().replace("Predicted serotype", "value"): record[1].strip() for record in
+               [line.split(":") for line in sys.stdin.readlines() if line.strip() != '']})
+print(json.dumps(result))
