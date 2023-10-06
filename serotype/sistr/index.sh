69ebc136f5ac1fe86097d7b9ca17c75c6f80b678
CorinYeatsCGPS <107122521+CorinYeatsCGPS@users.noreply.github.com>
2023-10-06 10:54:17 +0100

    Sistr 1.1.2 update (#5)
    * Since the base image we use had not been updated I've added a new Dockerfile that is able to build v1.1.2. The old version has been kept as Dockerfile.simple as the base image may be updated in the future and would be more robust. The new image is also 50MB smaller (wow).
    * Also removed logging noise from the production index.sh.

--- serotype/sistr/index.sh
+++ serotype/sistr/index.sh
@@ -2,5 +2,5 @@
 
 cat - > /tmp/input.fasta
 
-sistr --qc /tmp/input.fasta | \
+sistr --qc /tmp/input.fasta 2> /dev/null | \
   /opt/pathogenwatch/jq-linux64 '{ value: .[0].serovar, subspecies: .[0].cgmlst_subspecies, source: "SISTR" }'
