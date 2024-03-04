40518500daa6536e6576c5678c15ef598dc5dbed
Corin Yeats <corin.yeats@cgps.group>
2024-03-04 08:55:41 +0000

    Prevents the organism environment variables being overwritten by the PW_ versions if they already exist. Allows forwards/backwards compatibility.

--- mlst/entrypoint.sh
+++ mlst/entrypoint.sh
@@ -2,9 +2,11 @@
 
 set -e -o pipefail
 
-export ORGANISM_TAXID=$PW_ORGANISM_TAXID
-export SPECIES_TAXID=$PW_SPECIES_TAXID
-export GENUS_TAXID=$PW_GENUS_TAXID
+if [[ -z "ORGANISM_TAXID" ]]; then
+  export ORGANISM_TAXID=$PW_ORGANISM_TAXID
+  export SPECIES_TAXID=$PW_SPECIES_TAXID
+  export GENUS_TAXID=$PW_GENUS_TAXID
+fi
 
 /usr/local/bin/node --max-old-space-size=4096 /usr/local/mlst/index.js | \
   /usr/local/bin/node /pathogenwatch/formatter.js "$PW_SPECIES_TAXID" "$PW_TASK"
