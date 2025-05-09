74d27161a55956f07b3a2fd85d642bed1ab2db08
Corin Yeats <corin.yeats@cgps.group>
2025-05-09 17:40:44 +0100

    Fixed MLST entrypoint.sh to use the sanitiser.

--- mlst/entrypoint.sh
+++ mlst/entrypoint.sh
@@ -1,6 +1,22 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -e -o pipefail
+set -euo pipefail
 
-/usr/local/bin/node --max-old-space-size=4096 /usr/local/mlst/index.js | \
-  /usr/local/bin/node /pathogenwatch/formatter.js
+# Function to report errors
+error_report() {
+    echo "Error: Command failed: $BASH_COMMAND" >&2
+}
+
+trap 'error_report' ERR
+
+# Create the encoded FASTA
+if ! sanitiser --store /mapping_store encode - > /tmp/tmp.fa; then
+    echo "Error: Failed to create encoded FASTA file." >&2
+    exit 1
+fi
+
+# Run Kaptive
+if  ! cat /tmp/tmp.fa | /usr/local/bin/node --max-old-space-size=4096 /usr/local/mlst/index.js | sanitiser --store /mapping_store decode - | /usr/local/bin/node /pathogenwatch/formatter.js; then
+    echo "Error: Failed to run." >&2
+    exit 1
+fi
